8918fb52fa238d7b8db2a5ff21b26880989ef1a2
jendiamond <thejendiamond@gmail.com>
2020-07-17 15:18:53 -0700

    VRSUS-19 Create SCSS structure

--- src/App.vue
+++ src/App.vue
@@ -10,24 +10,10 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import "styles/legacy/old_blacklight";
+// @import "styles/slider";
+// @import "styles/blacklight_range_limit/blacklight_range_limit";
+@import "styles/legacy/ursus";
+@import "styles/base/base";
+@import "styles/theme_ursus/theme_ursus";
 </style>
